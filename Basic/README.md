b30d7c1ce4e14a690063577ed6a6e3597c264ce5
yjiewei <yang_7131@163.com>
2022-08-31 17:18:34 +0800

    feat(Arrays.asList把数据转换为 List坑):1.不能直接使用Arrays.asList 来转换基本类型数组

--- Basic/README.md
+++ Basic/README.md
@@ -1,4 +1,11 @@
-### 回顾Java基础知识
+### Java基础知识
 - 枚举 enum : 类的对象只有有限个，确定的；定义一组常量时
 - 注解 annotation
 - 其他知识点
+
+### Java业务常见错误
+- 极客时间课程：https://time.geekbang.org/column/article/216778
+- 具体最好看代码，代码写了很多注释 com.yjiewei.businesserror
+
+### 编码问题
+1. 用了lombok的@Slf4j但用日志的时候，没有log.info()方法，https://www.cnblogs.com/756623607-zhang/p/16183715.html
---
+++ Basic/src/main/java/com/yjiewei/businesserror/BusinessErrorTest10.java
@@ -0,0 +1,45 @@
+package com.yjiewei.businesserror;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * List、Set、Map、Queue 可以从广义上统称为集合类数据结构，可以分为 Map 和 Collection 两大类。
+ * 把数组转换为 List 集合、对 List 进行切片操作、List 搜索的性能问题等几个方面着手，来聊聊其中最可能遇到的一些坑
+ * @author yangjiewei
+ * @date 2022/8/31
+ */
+@Slf4j
+public class BusinessErrorTest10 {
+
+    /**
+     * Arrays.asList可以把原始的数组转换成list，就可以开展Java8中各种stream操作了。
+     * Arrays.asList 把数据转换为 List 的三个坑
+     * 1.不能直接使用 Arrays.asList 来转换基本类型数组
+     */
+    @Test
+    public void test1() {
+        int[] arr = {1, 2, 3};
+
+        log.info("1.直接用Arrays.asList转换数组为list");
+        List list = Arrays.asList(arr);
+        // 坑1：这里记录的是 int数组，只有一个元素，元素类型是整数数组
+        // 原因：只能是把 int 装箱为 Integer，不可能把 int 数组装箱为 Integer 数组，所以这里转数组的时候，把int[]作为类型了
+        log.info("list:{} size:{} class:{}", list, list.size(), list.get(0).getClass());
+
+        // 解决方法：1.用Arrays.stream()方法转换
+        log.info("2.用Arrays.stream方法将数组转化为list");
+        IntStream intStream = Arrays.stream(arr);
+        intStream.forEach((a)-> log.info(String.valueOf(a)));
+
+        // 解决方法：2.不要用int数组，声明Integer数组
+        log.info("3.定义Integer数组，而不用int数组");
+        Integer[] arr1 = {1, 2, 3};
+        List<Integer> integerList = Arrays.asList(arr1);
+        log.info("integerList:{} size:{} class:{}", integerList, integerList.size(), integerList.get(0).getClass());
+    }
+}
